6ee5f27cfa131ac362a4ce6e0734945bec974037
s0mm1er <quanhoaquarry@gmail.com>
2026-02-22 09:39:24 +0700

    feat: modify egg fall to manual drop,  remove the gravity
    Remove the gravity from egg drop, the drop will now have statable speed

--- Assets/Scripts/EggScript.cs
+++ Assets/Scripts/EggScript.cs
@@ -1,37 +1,67 @@
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 public class EggScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private Animator _animator;
-        
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _fallSpeed = 5f;
+
+    private bool _isBreaking = false;
+
+    private void Awake()
+    {
+        if (_rb == null)
+            _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        // Reset trạng thái khi spawn (quan trọng nếu dùng Object Pool)
+        _isBreaking = false;
+        _rb.bodyType = RigidbodyType2D.Dynamic;
+        _rb.linearVelocity = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isBreaking)
+        {
+            // Ép vận tốc cố định xuống dưới
+            _rb.linearVelocity = Vector2.down * _fallSpeed;
+        }
+    }
+
+    private void Start()
     {
         StartCoroutine(CheckEggPosition());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    IEnumerator CheckEggPosition()
+    private IEnumerator CheckEggPosition()
     {
         while (true)
         {
             Vector3 viewPort = Camera.main.WorldToViewportPoint(transform.position);
-            if(viewPort.y < 0.05)
+
+            if (viewPort.y < 0.05f && !_isBreaking)
             {
-                _animator.SetTrigger("Break");
-                _rb.bodyType = RigidbodyType2D.Static;
-                Destroy(gameObject, 1);
-                break;
+                BreakEgg();
+                yield break;
             }
+
             yield return null;
         }
+    }
+
+    private void BreakEgg()
+    {
+        _isBreaking = true;
+
+        _rb.linearVelocity = Vector2.zero;
+        _rb.bodyType = RigidbodyType2D.Static;
+
+        _animator.SetTrigger("Break");
+
+        Destroy(gameObject, 1f);
     }
 }
